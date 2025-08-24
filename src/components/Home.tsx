7817519467ca84f3d713ccb450128f4fedb6aa3b
s2terminal <suzuki.sh@s2terminal.com>
2025-08-24 18:21:13 +0900

    add mui

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,11 @@
+import Button from '@mui/material/Button';
+
 export const Home = () => {
   return (
     <div>
       <h1>Welcome to the Astro Project</h1>
       <p>This is the index page.</p>
+      <Button variant="contained">Hello world</Button>
     </div>
   );
 };
